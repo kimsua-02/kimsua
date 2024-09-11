7f0062f4ab1023daa4bfd3fbc079d736d82bde48
kimsua <k021113a@naver.com>
2024-09-11 19:58:52 +0900

    *

---
+++ kiosk/javascript.js
@@ -0,0 +1,11 @@
+let menus = [0, 0, 0, 0, 0, 0];
+
+let sum = 0;
+let choice = 0;
+
+function addToCart(menu, won){
+    const emptyCart = document.querySelector("#empty-cart");
+    const cart = document.querySelector("#cart");
+
+    emptyCart
+}
